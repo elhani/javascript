f088e470611533029b51f0c698dca35dcb763711
elhani <52415112+elhani@users.noreply.github.com>
2019-07-10 13:05:01 +0200

    Add files via upload
    ifelse

---
+++ ifelse.js
@@ -0,0 +1,10 @@
+var ageAsString =promt("what is your age","");
+var age= Number(ageAsString);
+if (age<40)
+{
+    alert("oh you are so young");
+}
+else{
+    alert("don't worry,you'are young at heat");
+}
+alert ("thank you");
