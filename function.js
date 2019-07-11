39c3abb0394e340b6d8285ffbb27c865ee1d491d
elhani <52415112+elhani@users.noreply.github.com>
2019-07-11 14:46:53 +0200

    Update function.js
    modif function

--- function.js
+++ function.js
@@ -1,7 +1,7 @@
+   var totalPrice =calculTotalPrice(2,4,00);
 function calculTotalPrice(quantity,price,currency)
 {
     return quantity*price;
 } ;
-var totalPrice =calculTotalPrice(2,4,00);
 
 console.log(totalPrice);
