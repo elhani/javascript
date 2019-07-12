f89d4ceec06398c216c4ea945e6df15af9569fa9
elhani <52415112+elhani@users.noreply.github.com>
2019-07-12 18:45:06 +0200

    Add files via upload
    variable type scope

---
+++ v-t-s.js
@@ -0,0 +1,16 @@
+console.log("the orde is \" In Progress\"");
+console.log('nike\'s order');
+
+
+var productType ='hardware';
+
+console.log(productType.length);
+console.log('['  + productType.trim() + ']')
+console.log(productType.toLowerCase());
+console.log(productType.toUpperCase());
+console.log(Boolean(0));
+console.log(Boolean());
+console.log(Boolean(" "));
+console.log(Boolean(6));
+var value = 12.12;
+console.log(typeof !!value);
