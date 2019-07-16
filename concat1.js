c0bc1968e61bc626244ff1a73cad9b79c2a5381e
elhani <52415112+elhani@users.noreply.github.com>
2019-07-16 18:00:20 +0200

    Add files via upload
    concat1

---
+++ concat1.js
@@ -0,0 +1,12 @@
+var ratings =[1,2,3];
+var newRatings = ratings.concat([4,5,6],7,8);
+newRatings.shift(); // supprimer premier valeur ...le contraire de pop 
+console.log(newRatings);// nombre de valeur debut de zéro 
+console.log(newRatings.length);// longueur
+console.log(newRatings.values());// type 
+console.log(newRatings.valueOf());// affiche tab avec accolade
+
+
+
+
+
