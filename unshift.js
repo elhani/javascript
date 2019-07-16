5e2a7d2f9c6fbf002a9fc499d1a9d346334b9f71
elhani <52415112+elhani@users.noreply.github.com>
2019-07-16 17:52:32 +0200

    Add files via upload
    ajout au premier

---
+++ unshift.js
@@ -0,0 +1,11 @@
+
+//ajouter un ou plusier element et renvoie la longueur du tab
+
+var ratings = [];
+ratings.push(5);
+ratings.push(2,4);
+
+var newcount = ratings.unshift(3) ;
+console.log(ratings);
+console.log (ratings[0]);
+console.log(newcount);
