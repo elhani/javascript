8cbac9018a8695ab51e9581d28663849d992f305
elhani <52415112+elhani@users.noreply.github.com>
2019-07-17 18:43:23 +0200

    Add files via upload

---
+++ field.js
@@ -0,0 +1,17 @@
+
+
+
+ var project = new Object();
+
+        project.name ='Project phoenix';
+        project.securitylevel = '15';
+        project.updateDueDate = function ()
+       {
+
+               return true ;
+        };
+   
+ var field = 'securitylevel' ;
+ console.log(project[field]);
+
+  
