d1a1b434d29ac4e9fd943a8695303a413621234b
elhani <52415112+elhani@users.noreply.github.com>
2019-07-18 18:36:09 +0200

    Add files via upload

---
+++ proto1.js
@@ -0,0 +1,14 @@
+
+var eleve =
+   {
+    name :'sami'
+   };
+var NAME = Object.create(eleve);
+console.log(NAME.name);
+
+ console.log( typeof(NAME)==="object");
+
+ console.log(typeof NAME.name);
+ 
+
+
